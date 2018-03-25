022f68fb5a247b41d1cd622ad63d1d74283dc960
frankc60 <frankc60@gmail.com>
2018-03-25 17:44:18 +0100

    update sample

--- sample.js
+++ sample.js
@@ -30,6 +30,6 @@ c.start("a"); //start timer
 
 setTimeout(()=>{c.end("a")},5525); //stop timers - after 5.5 seconds
 
-c.lg("hello world",()=>{
-  console.log("hi");
+c.lg("doing DB lookup",()=>{
+  //retrieve data from db
 })
